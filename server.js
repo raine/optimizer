352378430b62ce53b412c8d3afa21edac3da8e3b
Raine Virta <raine.virta@gmail.com>
2014-05-11 20:30:54 +0300

    optimize: handle bad mimetypes

--- server.js
+++ server.js
@@ -54,32 +54,41 @@ function fail(res) {
   };
 }
 
+var middlewares = {
+  'image/png': [
+    Imagemin.pngquant(),
+    Imagemin.optipng({ optimizationLevel: 0 })
+    // zopfli()
+  ],
+  'image/jpeg': [
+    Imagemin.jpegtran({ progressive: true })
+  ]
+};
+
+var acceptedType = _.partial(_.contains, _.keys(middlewares));
+
 function optimize(file_path, mime) {
-  return tmp.fileAsync({
-    mode    : 0644,
-    postfix : path.extname(file_path)
-  }).spread(function(tmp_path) {
-    var imagemin = new Imagemin()
-      .src(file_path)
-      .dest(tmp_path);
+  return new Promise(function(resolve, reject) {
+    if (!acceptedType(mime)) {
+      return reject('Invalid mime type');
+    }
 
-    _.each(middlewares(mime), function(ware) {
-      imagemin.use(ware);
+    tmp.fileAsync({
+      mode    : 0644,
+      postfix : path.extname(file_path)
+    }).spread(function(tmp_path) {
+      var imagemin = new Imagemin()
+        .src(file_path)
+        .dest(tmp_path);
+
+      _.each(middlewares[mime], function(ware) {
+        imagemin.use(ware);
+      });
+
+      Promise.props({
+        dest: tmp_path,
+        file: Promise.promisify(imagemin.optimize, imagemin)()
+      }).then(resolve);
     });
-
-    return Promise.promisify(imagemin.optimize, imagemin)();
   });
-}
-
-function middlewares(mime) {
-  return {
-    'image/png': [
-      Imagemin.pngquant(),
-      // Imagemin.optipng({ optimizationLevel: 3 })
-      zopfli()
-    ],
-    'image/jpg': [
-      Imagemin.jpegtran({ progressive: true })
-    ]
-  }[mime];
 }
