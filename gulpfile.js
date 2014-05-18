a1a7383d83ca03d107b94ab82307f0185ca9bada
Raine Virta <raine.virta@gmail.com>
2014-05-18 14:30:45 +0300

    gulp: comment on stylus

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ var gulp = require('gulp')
 gulp.task('livereload', function() {
   var lr = livereload(35750);
   var reload = function(file) {
+    // stylus middleware compiles the .styl files, so livereload needs to
+    // think it was actually a css file that changed, because otherwise it
+    // will reload the page instead of just loading the new css.
+    // maybe a better option would be compiling stylus with gulp
     if (path.extname(file.path) === '.styl') {
       file.path = __dirname + '/public/main.css';
     }
