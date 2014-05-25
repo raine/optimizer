4c4e963e315c50853c05cc15ba593a4a1a40203a
Raine Virta <raine.virta@gmail.com>
2014-05-25 17:36:03 +0300

    FileTableRow: wait for animation to complete

--- app/js/components/filetablerow.js
+++ app/js/components/filetablerow.js
@@ -3,7 +3,23 @@ var React = require('react/addons')
   , cx = React.addons.classSet
 ;
 
+var transitionEndEvent = whichTransitionEvent();
+
+// TODO: Extract progress bar to a separate component
 var FileTableRow = React.createClass({
+  getInitialState: function() {
+    return {
+      progAnimReady: false
+    };
+  },
+
+  componentDidMount: function() {
+    var progInner = this.refs.progInner.getDOMNode();
+    progInner.addEventListener(transitionEndEvent, function() {
+      this.setState({ progAnimReady: true });
+    }.bind(this));
+  },
+
   render: function() {
     var file = this.props.file;
     var progress = {
@@ -12,7 +28,7 @@ var FileTableRow = React.createClass({
 
     var progClass = cx({
       'progress': true,
-      'progress-striped active': file.state === 'OPTIMIZING'
+      'progress-striped active': file.state === 'OPTIMIZING' && this.state.progAnimReady
     });
 
     var progInnerClass = cx({
@@ -26,6 +42,7 @@ var FileTableRow = React.createClass({
         <td>
           <div className={progClass}>
             <div
+              ref="progInner"
               className={progInnerClass}
               role="progressbar"
               style={progress}
@@ -40,3 +57,20 @@ var FileTableRow = React.createClass({
 });
 
 module.exports = FileTableRow;
+
+function whichTransitionEvent() {
+  var t;
+  var el = document.createElement('fakeelement');
+  var transitions = {
+    'transition'       : 'transitionend',
+    'OTransition'      : 'oTransitionEnd',
+    'MozTransition'    : 'transitionend',
+    'WebkitTransition' : 'webkitTransitionEnd'
+  }
+
+  for (t in transitions) {
+    if (el.style[t] !== undefined) {
+      return transitions[t];
+    }
+  }
+}
